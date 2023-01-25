8bb0684d30b4411770e63b361a35a94f127a1fc6
James.Harrison <James.Harrison@Serko.com>
2023-01-25 17:12:08 +1300

    Add starting point for StringCalculator

---
+++ 2.StringCalculator/index.js
@@ -0,0 +1,6 @@
+const stringCalculator = () => {
+  //Add your function to the return object
+  return {};
+};
+
+module.exports = stringCalculator;
---
+++ 2.StringCalculator/index.spec.js
@@ -0,0 +1,10 @@
+const stringCalculator = require('./index');
+
+describe('StringCalculator tests', () => {
+  // This is just a starting point. Delete or update this test.
+  test('Given x should do y', () => {
+    const result = stringCalculator();
+
+    expect(result).not.toBeNull();
+  });
+});
